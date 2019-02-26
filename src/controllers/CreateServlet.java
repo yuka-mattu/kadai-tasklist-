e97c29c5d1d30403d1fcd95e134fa7a74f6dd31a
yuka.m <yuka-m@brainnet.co.jp>
2019-02-26 17:21:43 +0900

    Add flush message

--- src/controllers/CreateServlet.java
+++ src/controllers/CreateServlet.java
@@ -48,9 +48,11 @@ public class CreateServlet extends HttpServlet {
             m.setCreated_at(currentTime);
             m.setUpdated_at(currentTime);
 
+            // データベースに保存
             em.getTransaction().begin();
             em.persist(m);
             em.getTransaction().commit();
+            request.getSession().setAttribute("flush", "登録が完了しました。");
             em.close();
 
             response.sendRedirect(request.getContextPath() + "/index");
